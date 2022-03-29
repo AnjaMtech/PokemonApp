28eed0119af8bbe54b5d6e9176d8f8ea5e9b40c7
Anja Schmidt <anjaschmidt@Anjas-MacBook-Air.local>
2022-03-29 09:01:57 -0600

    added randomization of setting board with various pokemon pairs

--- js/main.js
+++ js/main.js
@@ -4,21 +4,66 @@ WIP means Work in Progress. Please surround code you are modfiying with the labe
 
 -----NOTES FOR PARTNER-----*/
 let cardSize = 40;
-
-
+//-----TEMP-----
+const pokemon = [
+  {
+    name: "Pikachu",
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
+  },
+  {
+    name: "Machamp",
+    url: "https://static.wikia.nocookie.net/pokemon/images/6/60/Team_Rocket_Machamp.png/revision/latest?cb=20200112112729"
+  },
+  {
+    name: "Electrike",
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/309.png"
+  },
+  {
+    name: "Ditto",
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/132.png"
+  },
+  {
+    name: "Dugtrio",
+    url: "https://static.wikia.nocookie.net/pokemon/images/c/c7/Katie_Dugtrio.png/revision/latest?cb=20150819064319"
+  },
+  {
+    name: "Torchic",
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/255.png"
+  },
+  {
+    name: "Rattata",
+    url: "https://spng.pngfind.com/pngs/s/686-6866018_pokemon-rattata-png-transparent-png.png"
+  },
+  {
+    name: "Squirtle",
+    url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+  }
+]
+//-----TEMP-----
 
 //-----WIP-----
 window.onload = function(){
-  setBoard(5, 3);
+  setBoard(6, 3);
 }
 
 function setBoard(x, y){
+  let myCards =[];
+  for(let i=0; i<x*y/2; i++){
+    let currPokemon = Math.floor(Math.random()*pokemon.length);
+    for(let j=0; j<2; j++){
+      myCards.splice(Math.floor(Math.random()*myCards.length), 0, currPokemon);
+    }
+  }
+
   //clears board of any leftover cards
   $("#card-board").empty();
   //Adds cards to board
   for(let i=0; i<x*y; i++){
     $("#card-board").append(
-      `<div class="card"></div>`
+      `<div class="card">
+        <h2>${pokemon[myCards[i]].name}</h2>
+        <img src="${pokemon[myCards[i]].url}" alt="Placeholder" class="img-fluid">
+      </div>`
     );
   }
   //Makes card sizes all exact dimensions
