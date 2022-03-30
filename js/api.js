ae112fe54d710f1a4e622b73b00e5c5253120f2d
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-30 11:51:06 -0600

    tested a few different methods to pull information from the api

--- js/api.js
+++ js/api.js
@@ -1,15 +1,28 @@
-function getAvailableCourses() {
-	fetch("https://golf-courses-api.herokuapp.com/courses")
+//created items
+const pokemonApi = "https://api.pokemontcg.io/v2/cards/data/<id>";
+const pokemonName = document.querySelector("test-container");
+const testDisplay = document.querySelector("testBtn");
+
+//Event listners
+// test for data response testDisplay.addEventListener("click", getPokemon);
+
+//this function grabs the pokemon data via the API
+function getPokemon() {
+	return fetch(pokemonApi, {
+		//method: "GET",
+		headers: {
+			"X-Api-Key": "ef72570ff371408f9668e414353b7b2e",
+		},
+	})
 		.then((response) => {
-			//passes api into this function
-			return response.json(); //returns the api information to .JSON
+			//returns api data in json format
+			return response.json();
 		})
-		.then((info) => {
-			//takes the .JSON and grabs the data from the .JSON
-			myCourses = info; // Assigns the .JSON data to variable myCourses
-			console.log(info);
+		.then((pokemon) => {
+			pokemon.card.find("base1-4");
 		})
-		.then(() => {
-			selectCourse(); // calls the select course function
+		.then((card) => {
+			return card.name;
+			console.log(card.name); // "Charizard"
 		});
 }
