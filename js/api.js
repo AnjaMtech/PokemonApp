399a8dc8647eb5c8230cc59f0b5d47e372a372b0
nolangh <nolan.higgins2245@stu.mtec.edu>
2022-03-31 09:32:46 -0600

    -a

--- js/api.js
+++ js/api.js
@@ -1,13 +1,15 @@
-const pokemonApi = "https://api.pokemontcg.io/v2/cards/data/<id>";
-const pokemonName = document.querySelector("test-container");
-const testDisplay = document.querySelector("testBtn");
+//TODO make start page
 
-//this function grabs the pokemon data via the API
-function getPokemon() {
-	return fetch(pokemonApi, {
+const apiCards = "https://api.pokemontcg.io/v2/cards/data/<id>";
+const apiSets = "https://api.pokemontcg.io/v2/sets";
+const apiKey = "ef72570ff371408f9668e414353b7b2e";
+
+//This function grabs the cards
+function getCards() {
+	return fetch(apiCards, {
 		method: "GET",
 		headers: {
-			"X-Api-Key": "ef72570ff371408f9668e414353b7b2e", // api key
+			"X-Api-Key": apiKey,
 		},
 	})
 		.then((response) => {
@@ -17,9 +19,61 @@ function getPokemon() {
 			pokemon.card.find("base1-4");
 		})
 		.then((card) => {
-			return card.name;
 			console.log(card.name); // "Charizard"
+			return card.name;
 		});
 }
 
-//ANCHOR I reworked this like 100 times but it keeps throwing a 404 error. i'll keep working it over though
+let sets;
+
+function getSets() {
+	return fetch(apiSets, {
+		method: "GET",
+		headers: {
+			"X-Api-Key": apiKey,
+		},
+	})
+		.then((res) => {
+			res.json();
+		})
+		.then((setInfo) => {
+			sets = setInfo;
+			console.log(setInfo);
+		})
+		.then(() => {
+			selectSet();
+		});
+}
+
+function selectSet() {} //TODO finish this function
+
+//ANCHOR Ignore anything below this comment. Using it as reference code
+
+let myCourses = "";
+
+function getAvailableCourses() {
+	fetch("https://golf-courses-api.herokuapp.com/courses")
+		.then((response) => {
+			//passes api into this function
+			return response.json(); //returns the api information to .JSON
+		})
+		.then((info) => {
+			//takes the .JSON and grabs the data from the .JSON
+			myCourses = info; // Assigns the .JSON data to variable myCourses
+			console.log(info);
+		})
+		.then(() => {
+			selectCourse(); // calls the select course function
+		});
+}
+
+function selectCourse() {
+	let courseOptionsHtml = "";
+	myCourses.courses.forEach((course) => {
+		courseOptionsHtml += `<option value="${course.id}">${course.name}</option>`;
+		courseImages += `<h4>${course.name}</h4><img id= 'course-image' src= '${course.image}'>`;
+	});
+	// document.addEventListener("click", getCourseImage);
+	document.getElementById("course-select").innerHTML = courseOptionsHtml;
+	document.getElementById("course-image").innerHTML = courseImages; //make the image only populate with an onClick("")
+}
