8d29c7c7e359479ac8a184cfd1ac32c04fce9c9b
AnjaMtech <97115265+AnjaMtech@users.noreply.github.com>
2022-04-05 08:30:01 -0600

    API meging to master branch
    * -a
    
    * worked out the get card api to only pull 16 cards
    
    * added start page and start page css
    
    * -a
    
    * finished API
    
    Co-authored-by: nolangh <nolan.higgins2245@stu.mtec.edu>
    Co-authored-by: nolangh <nolanghiggins@gmail.com>

--- js/api.js
+++ js/api.js
@@ -1,25 +1,59 @@
-const pokemonApi = "https://api.pokemontcg.io/v2/cards/data/<id>";
-const pokemonName = document.querySelector("test-container");
-const testDisplay = document.querySelector("testBtn");
+const apiCards = "https://api.pokemontcg.io/v2/cards?page=1&pageSize=32";
+const apiSets = "https://api.pokemontcg.io/v2/sets";
+const apiKey = "ef72570ff371408f9668e414353b7b2e";
+let cardSets = [];
 
-//this function grabs the pokemon data via the API
-function getPokemon() {
-	return fetch(pokemonApi, {
+//This function grabs the cards
+function getCards() {
+	return fetch(apiCards, {
 		method: "GET",
 		headers: {
-			"X-Api-Key": "ef72570ff371408f9668e414353b7b2e", // api key
+			"X-Api-Key": apiKey,
 		},
 	})
 		.then((response) => {
-			response.json();
+			return response.json();
 		})
 		.then((pokemon) => {
-			pokemon.card.find("base1-4");
-		})
-		.then((card) => {
-			return card.name;
-			console.log(card.name); // "Charizard"
+			let cards = pokemon;
+			console.log(cards);
 		});
 }
 
-//ANCHOR I reworked this like 100 times but it keeps throwing a 404 error. i'll keep working it over though
+//this function grabs the sets
+
+function getSets() {
+	return fetch(apiSets, {
+		method: "GET",
+		headers: {
+			"X-Api-Key": apiKey,
+		},
+	})
+		.then((res) => {
+			return res.json();
+		})
+		.then((setInfo) => {
+			cardSets = setInfo;
+			console.log(setInfo);
+		})
+		.then(() => {
+			selectSet();
+		});
+}
+
+function selectSet() {
+	let testSelectHtml = "";
+	cardSets.data.forEach((data) => {
+		console.log(data);
+		testSelectHtml += `<option value="${data.id}">${data.name}</option>`;
+	});
+
+	document.getElementById("selectSet").innerHTML = testSelectHtml;
+}
+
+function render() {
+	getSets();
+	getCards();
+}
+
+render();
